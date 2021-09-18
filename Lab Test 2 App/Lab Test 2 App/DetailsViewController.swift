4f503a04608c83d75474b675931ff5378e689e0d
omairys <hello.omug@gmail.com>
2021-09-17 23:16:08 -0400

    Storyboard version 1

---
+++ Lab Test 2 App/Lab Test 2 App/DetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.swift
+//  Lab Test 2 App
+//
+//  Created by Omairys Uzcátegui on 2021-09-17.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
